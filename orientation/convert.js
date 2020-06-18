d51e4b4ed365c78acabc3222899e1a35bdce7d71
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2020-06-18 14:24:48 -0400

    Add Kroki support for diagrams.

--- orientation/convert.js
+++ orientation/convert.js
@@ -1,8 +1,11 @@
 // Load Asciidoctor.js and the reveal.js converter
 var asciidoctor = require('@asciidoctor/core')()
+var kroki = require('asciidoctor-kroki')
+kroki.register(asciidoctor.Extensions)
 var asciidoctorRevealjs = require('@asciidoctor/reveal.js')
 asciidoctorRevealjs.register()
-
+var registry = asciidoctor.Extensions.create()
+kroki.register(registry)
 // Convert the document 'presentation.adoc' using the reveal.js converter
 var options = { safe: 'safe', backend: 'revealjs' }
 asciidoctor.convertFile('orientation.adoc', options)
