bbe8e30018845a83cddab8313342d3d535561452
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2020-07-08 23:01:01 -0400

    Add "What is Eclipse" presentation
    Signed-off-by: Wayne Beaton <wayne.beaton@eclipse-foundation.org>

---
+++ eclipse_ide/convert.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Eclipse Foundation, Inc. and others. 
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ */
+// Load Asciidoctor.js and the reveal.js converter
+var asciidoctor = require('@asciidoctor/core')()
+var kroki = require('asciidoctor-kroki')
+kroki.register(asciidoctor.Extensions)
+var asciidoctorRevealjs = require('@asciidoctor/reveal.js')
+asciidoctorRevealjs.register()
+var registry = asciidoctor.Extensions.create()
+kroki.register(registry)
+// Convert the document 'presentation.adoc' using the reveal.js converter
+var options = { safe: 'safe', backend: 'revealjs' }
+asciidoctor.convertFile('ide.adoc', options)
